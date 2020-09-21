ec4068b3c66c975b78013f485bfce70f6a72cafe
Hydrochlorick <hydrochlorick@icloud.com>
2020-09-21 13:58:56 -0700

    Added removeItem function. Currently functions are never called and buttons do nothing.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -3,36 +3,39 @@ import data from './data.js'
 const itemsContainer = document.getElementById('items')
 
 for (let i=0; i<data.length; ++i) {
-    let newDiv = document.createElement('div');
-      newDiv.className = 'item'
-    let img = document.createElement('img');
-    img.src = data[i].image
-    img.width = 300
-    img.height = 300
-    newDiv.appendChild(img)
+  // Create a new Div for every data item
+  let newDiv = document.createElement('div');
+  // Give it the class name "item"
+    newDiv.className = 'item'
+  // Create the img element with fixed size from data object
+  let img = document.createElement('img')
+  img.src = data[i].image
+  img.width = 300
+  img.height = 300
+  // Add img tag as a child of the new Div
+  newDiv.appendChild(img)
   
-    let desc = document.createElement('P')
-    desc.innerText =data[i].desc
-    newDiv.appendChild(desc)
-    let price = document.createElement('P')
-    price.innerText = data[i].price
-    newDiv.appendChild(price)
+  let desc = document.createElement('P')
+  desc.innerText = data[i].desc
+  newDiv.appendChild(desc)
+  let price = document.createElement('P')
+  price.innerText = data[i].price
+  newDiv.appendChild(price)
   
-    let button = document.createElement('button')
-    button.id = data[i].name
+  let button = document.createElement('button')
+  button.id = data[i].name
   
-    button.dataset.price = data[i].price
-    button.innerHTML = "Add to Cart"
-    newDiv.appendChild(button)
-    itemsContainer.appendChild(newDiv)
-  }
+  button.dataset.price = data[i].price
+  button.innerHTML = "Add to Cart"
+  newDiv.appendChild(button)
+  itemsContainer.appendChild(newDiv)
+}
 
 
 const cart = []
 
-const obj = {}
-
-funtion addItem(name, price) {
+// Add item -------------------------------------------------
+function addItem(name, price) {
   for (let i = 0; i < cart.length; i +=1 ) {
     if (cart[i].name === name) {
       cart[i].qty += 1
@@ -41,6 +44,20 @@ funtion addItem(name, price) {
   }
   const item = {name, price, qty: 1}
   cart.push(item)
+}
+
+function remoteItem(name, qty = 0) {
+  for (let i =0; i < cart.length; i += 1) {
+    if (cart[i].name === name) {
+      if (qty > 0) {
+        cart[i].qty -= qty
+      }
+      if (cart[i].qty < 1 || qty ===0) {
+        cart.splice(i, 1)
+      }
+      return
+    }
+  }
 }
 
 function showItems() {
